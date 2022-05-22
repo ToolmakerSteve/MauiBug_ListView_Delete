07ce71832ce82bc2e9eb2a54ecfa763cf7f20507
ToolmakerSteve <ToolmakerSteve@users.noreply.github.com>
2022-05-22 13:28:05 -0500

    Initial commit

---
+++ TodoItem.cs
@@ -0,0 +1,14 @@
+﻿namespace MauiApp3
+{
+    public class TodoItem
+    {
+        public TodoItem(string todoText, bool complete)
+        {
+            TodoText = todoText;
+            Complete = complete;
+        }
+
+        public bool Complete { get; set; }
+        public string TodoText { get; }
+    }
+}
---
+++ TodoListViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace MauiApp3
+{
+    public class TodoListViewModel : BindableObject
+    {
+        public ObservableCollection<TodoItem> TodoItems { get; set; }
+
+        public TodoListViewModel()
+        {
+            TodoItems = new ObservableCollection<TodoItem>
+            {
+                //test
+                new TodoItem("todo 1", false),
+                new TodoItem("todo 2", true),
+                new TodoItem("todo 3", true),
+            };
+        }
+
+        string newTodoText = string.Empty;
+        public string NewTodoText
+        {
+            get { return newTodoText; }
+            set
+            {
+                if (value == newTodoText) return;
+                newTodoText = value;
+                OnPropertyChanged();
+            }
+
+        }
+
+        public ICommand AddTodoCommand => new Command(AddTodoItem);
+        public void AddTodoItem()
+        {
+            TodoItems.Add(new TodoItem(NewTodoText, false));
+
+            //clear entry and refocus
+            NewTodoText = string.Empty;
+        }
+
+        public ICommand RemoveTodoCommand => new Command(RemoveTodoItem);
+        void RemoveTodoItem(Object o)
+        {
+            TodoItem todoItemBeingRemoved = o as TodoItem;
+            TodoItems.Remove(todoItemBeingRemoved);
+        }
+    }
+}
+
